22cd7a01eca538336c6bf5c7444b614a8c8b2693
JoshuaPerkins <Perkins1220@hotmail.com>
2021-11-03 21:05:46 -0600

    a

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Perkins1220/synewave-dev.git master:gh-pages
+git push -f git@github.com:Perkins1220/synewave-dev.git main:gh-pages
 
 cd -
+
+read -rn1
